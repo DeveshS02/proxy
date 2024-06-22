b0b8a382ae1f1aeb470f6dac1cff54bd5e25fe8b
DeveshS02 <97830736+DeveshS02@users.noreply.github.com>
2024-06-22 13:42:19 +0530

    added new endpoints

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const corsOptions = {
     }
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 // List of endpoints from environment variable
 const endpoints = process.env.ENDPOINTS.split(',');
